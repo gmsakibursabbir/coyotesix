7cf1e0df332c8c9a83d3ebb2e8a9c6dc21fc9d9d
DESKTOP-3P213DA\sakibur <gmsakibursabbir@gmail.com>
2025-04-22 23:55:25 +0600

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+//menu
+const hamburgerButton = document.getElementById("hamburger-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamburgerButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
